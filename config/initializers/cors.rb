ced759ff66db1f443f1766548bf39c99bd4b41e2
Nihilus1999 <jose-elasmar1999@outlook.com>
2026-02-27 12:16:38 -0400

    update readme

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Aquí le decimos que permita peticiones desde cualquier origen ("*").
-    # Para entorno de desarrollo local, esto es lo más rápido y seguro para no tener bloqueos.
     origins "*"
-
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
